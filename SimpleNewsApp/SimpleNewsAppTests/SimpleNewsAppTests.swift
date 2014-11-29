d4242d06705aefdc9c2bc929270299ce0be8ac41
Linus Ling <360725966@qq.com>
2014-11-29 19:00:30 +0800

    Empty Project for SNA

---
+++ SimpleNewsApp/SimpleNewsAppTests/SimpleNewsAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleNewsAppTests.swift
+//  SimpleNewsAppTests
+//
+//  Created by Linus on 14-11-29.
+//  Copyright (c) 2014年 Linus. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SimpleNewsAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
